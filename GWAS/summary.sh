342f9a18d82f21dcb14cfe9091f71d54337b88eb
jjv4001 <jeyavandana@gmail.com>
2026-08-15 19:46:37 -0700

    Delete GWAS/summary.sh

--- GWAS/summary.sh
+++
@@ -1,8 +0,0 @@
-for set in merged
-do
-  for i in results/*.merged.cell_type_results.txt
-  do
-    j=$(basename $i .${set}.cell_type_results.txt)
-    awk -v j=$j -v s=$set 'NR!=1{print $0"\t"j"\t"s}' $i
-  done
-done > res.summary.tsv
